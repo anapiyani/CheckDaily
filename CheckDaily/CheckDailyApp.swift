f04d16be781cb800aa5e978ad46175c0edcee0ae
anapiyani <anapiyanurkeldi@gmail.com>
2025-09-23 19:00:15 +0500

    Initial Commit

---
+++ CheckDaily/CheckDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckDailyApp.swift
+//  CheckDaily
+//
+//  Created by Anapiya Nurkeldi on 23.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct CheckDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
